e7b8f26e63d07e773600110da6fc8e9b4736ad1e
ernii <ernii@amazon.com>
2018-11-14 10:05:56 -0500

    chore: Added Word example and updated link

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Regular Expressions
 
 ### Links
-#### https://www.youtube.com/watch?v=7DG3kCDx53c&list=PLRqwX-V7Uu6YEypLuls7iidwHMdCM6o2w
+#### [Coding Train](https://www.youtube.com/watch?v=7DG3kCDx53c&list=PLRqwX-V7Uu6YEypLuls7iidwHMdCM6o2w)
--- regex.pl
+++ regex.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 
+use diagnostics;
 use strict;
 use warnings;
-use diagnostics;
 our $VERSION = '0.0.1';
 
 my $str;        # String
@@ -15,4 +15,20 @@ $re      = qr/\d\d\d-\d\d\d\d/msx;
 if ( $str =~ $re ) {
     my $returnval =
       print "\n Example: $example\n  String: $str\n   Regex: $re\n\n";
+}
+
+$example = 'Word literal';
+$str     = 'bob';
+$re      = qr/bob/msx;
+if ( $str =~ $re ) {
+    my $returnval =
+      print " Example: $example\n  String: $str\n   Regex: $re\n\n";
+}
+
+$example = 'Word';
+$str     = 'bob';
+$re      = qr/\w/msx;
+if ( $str =~ $re ) {
+    my $returnval =
+      print " Example: $example\n  String: $str\n   Regex: $re\n\n";
 }
